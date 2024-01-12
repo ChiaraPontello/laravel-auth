4ca5d6908d95411e65c3fd38d57ed1bbec9e69b7
ChiaraPontello <chiarapontello1@gmail.com>
2024-01-12 16:12:53 +0100

    migrations + seeders

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -4,7 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use App\Models\Project;
+use Illuminate\Support\Str;
 class ProjectSeeder extends Seeder
 {
     /**
@@ -12,6 +13,16 @@ class ProjectSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $projects = config('db.posts');
+        foreach ($projects as $project) {
+            $newPost = new Project();
+            $newPost->image= $project['image'];
+            $newPost->title = $project['title'];
+            $newPost->body =$project['body'];
+            $newPost->user_id = 1;
+            $newPost->slug =Str::slug($project['title'], '-');
+
+            $newPost->save();
+        }
     }
 }
