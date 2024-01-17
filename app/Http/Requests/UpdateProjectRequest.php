572dd222128ed239e92af6d09c6fde436491fc58
ChiaraPontello <chiarapontello1@gmail.com>
2024-01-17 17:46:59 +0100

    UpdateProjectRequest function + use

--- app/Http/Requests/UpdateProjectRequest.php
+++ app/Http/Requests/UpdateProjectRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class UpdateProjectRequest extends FormRequest
 {
@@ -22,7 +23,20 @@ class UpdateProjectRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'title' => ['required', 'min:3', 'max:200', Rule::unique('projects')->ignore($this->project)],
+            'body' => ['nullable'],
+            'image' => ['nullable', 'url']
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'title.required' => 'Il titolo è obbligatorio',
+            'title.min' => 'Il titolo deve essere almeno :min caratteri',
+            'title.max' => 'Il titolo deve avere massimo :max caratteri',
+            'title.unique' => 'Questo titolo esiste già',
+            'image.url' => 'L\'immagine deve essere di tipo url'
         ];
     }
 }
