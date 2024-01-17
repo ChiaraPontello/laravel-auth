9f12630f43d2e21681df9f38ef67291bd23e4d9b
ChiaraPontello <chiarapontello1@gmail.com>
2024-01-17 18:42:46 +0100

    asset in edit.blade

--- resources/views/admin/projects/edit.blade.php
+++ resources/views/admin/projects/edit.blade.php
@@ -24,7 +24,10 @@
                 <div class="invalid-feedback">{{ $message }}</div>
             @enderror
         </div>
-
+        <div class="d-flex">
+            <div class="media me-4">
+                <img width="150" src="{{asset('storage/' . $progect->image)}}" alt="{{$project->title}}">
+            </div>
         <div class="mb-3">
             <label for="image">Image</label>
             <input type="file"  class="form-control @error('image') is-invalid @enderror" name="image" id="image" value="{{old('image', $project->image)}}"
@@ -33,9 +36,11 @@
                 <div class="invalid-feedback">{{ $message }}</div>
             @enderror
         </div>
-
+    </div>
+    <div class="mt-3">
         <button type="submit" class="btn btn-success">Save</button>
         <button type="reset" class="btn btn-primary">Reset</button>
+    </div>
     </form>
     </section>
 @endsection
